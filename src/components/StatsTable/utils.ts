286ebb8fed48940fa1594a2bc942fdbbf9ec80fd
Henzii <80477931+Henzii@users.noreply.github.com>
2025-03-05 13:54:10 +0200

    add prohibition switch

--- src/components/StatsTable/utils.ts
+++ src/components/StatsTable/utils.ts
@@ -1,16 +1,16 @@
 import { Game, Scorecard } from "@/types/game";
 
-const getTotalIncludeingHC = (sc: Scorecard, bHcMultiplier: number) =>
-    sc.total - sc.hc - sc.bHc * bHcMultiplier
+const getTotalIncludeingHC = (sc: Scorecard, bHcMultiplier: number, prohibition: boolean) =>
+    sc.total - sc.hc - sc.bHc * bHcMultiplier * (prohibition ? 0 : 1)
 
-const getPlusMinusIncludingHC = (sc: Scorecard, bHcMultiplier: number) =>
-    sc.plusminus - sc.hc - sc.bHc * bHcMultiplier
+const getPlusMinusIncludingHC = (sc: Scorecard, bHcMultiplier: number, prohibition: boolean) =>
+    sc.plusminus - sc.hc - sc.bHc * bHcMultiplier * (prohibition ? 0 : 1)
 
-const rankScorecards = (scorecards: Scorecard[], bHcMultiplier: number) => {
+const rankScorecards = (scorecards: Scorecard[], bHcMultiplier: number, prohibition: boolean) => {
     const ranks: number[] = []
     for (let i = 0; i < scorecards.length; i++) {
-        const score = getTotalIncludeingHC(scorecards[i], bHcMultiplier)
-        if (i > 0 && getTotalIncludeingHC(scorecards[i - 1], bHcMultiplier) == score) {
+        const score = getTotalIncludeingHC(scorecards[i], bHcMultiplier, prohibition)
+        if (i > 0 && getTotalIncludeingHC(scorecards[i - 1], bHcMultiplier, prohibition) == score) {
             ranks.push(ranks[i - 1])
         } else ranks.push(i + 1)
     }
@@ -48,11 +48,11 @@ export type MappedGames = {
 export const getResultForPlayer = (playerName: string, field: keyof Omit<MappedPlayer, 'name'>, mappedPlayers: MappedPlayer[]) =>
     mappedPlayers.find(player => player.name === playerName)?.[field] ?? ''
 
-export const parseGames = (games: Game[]): MappedGames => {
+export const parseGames = (games: Game[], prohibition: boolean): MappedGames => {
     const playerNames = getUniquePlayerNames(games)
     const competitions = games.map(game => {
-        const sortedScorecards = [...game.scorecards].sort((a, b) => getTotalIncludeingHC(a, game.bHcMultiplier) - getTotalIncludeingHC(b, game.bHcMultiplier))
-        const scorecardRanks = rankScorecards(sortedScorecards, game.bHcMultiplier)
+        const sortedScorecards = [...game.scorecards].sort((a, b) => getTotalIncludeingHC(a, game.bHcMultiplier, prohibition) - getTotalIncludeingHC(b, game.bHcMultiplier, prohibition))
+        const scorecardRanks = rankScorecards(sortedScorecards, game.bHcMultiplier, prohibition)
         const players = sortedScorecards.map((scorecard, index) => {
             return {
                 name: scorecard.user.name,
@@ -62,7 +62,7 @@ export const parseGames = (games: Game[]): MappedGames => {
                 rank: scorecardRanks[index],
                 points: Math.max(6 - scorecardRanks[index], 0),
                 beers: scorecard.beers,
-                hcPlusminus: getPlusMinusIncludingHC(scorecard, game.bHcMultiplier),
+                hcPlusminus: getPlusMinusIncludingHC(scorecard, game.bHcMultiplier, prohibition),
             }
         })
         return {
